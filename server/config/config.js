f06422bf1099aafb8278abefb9e1bf4fbcc9a9f6
Roberto Iglesias Cuesta <ricuesta1986@gmail.com>
2021-12-21 06:02:11 -0500

    Variable de Entorno

--- server/config/config.js
+++ server/config/config.js
@@ -19,6 +19,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
-    urlDB = 'mongodb+srv://cafe-user:Rroan.2017@cluster0.dc4mn.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URL;
+// urlDB = 'mongodb+srv://cafe-user:Rroan.2017@cluster0.dc4mn.mongodb.net/cafe?retryWrites=true&w=majority';
+
 
 process.env.URLDB = urlDB;
